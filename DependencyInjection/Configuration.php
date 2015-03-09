c57ab1223145f5dc0902fce59e138d799268ce87
Mlanawo Mbechezi <mlanawo.mbechezi@ikimea.com>
2015-03-10 00:01:01 +0100

    Fix sensio insight issues

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -28,7 +28,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('tempo_js_configuration')
+        $treeBuilder->root('tempo_js_configuration')
             ->children()
                 ->arrayNode('config_to_expose')
                     ->beforeNormalization()
